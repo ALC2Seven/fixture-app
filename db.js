18925c3783e93ae2ac4ff5bdc51a7a010688bd37
ALC2Seven <andrewcattley@gmail.com>
2026-06-04 07:26:50 +0100

    Fix Postgres SSL config for Railway

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 // Create all tables if they don't exist
